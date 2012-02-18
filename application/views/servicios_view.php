46809a618c1449acb1c53eed17af2f6b39c18cab
Pablo Garcia <pdgarcia@gmail.com>
2012-02-19 00:14:47 +0100

    code cleaning

--- application/views/servicios_view.php
+++ application/views/servicios_view.php
@@ -1,83 +1,77 @@
-		<div id="barrita"> 
-		</div>
-		
-		<div id="servicios">
-			<div id="foto_servicios">
-			<img src="<?php echo base_url('images/01.jpg'); ?>" width="300" height="300" alt="01">
-			</div>
-			<div id="texto_servicios">
-			<h5>INSTALACIÓN Y MANTENIMIENTO DE EQUIPOS INFORMÁTICOS Y REDES</h5>
-					No nos dedicamos a la simple distribución de equipos, sino que analizamos correctamente sus 
-					necesidades informáticas y realizamos un plan de compras con la vista puesta en el futuro: actualización, escalabilidad, rendimiento.</p> 
-				<p>Distribuimos primeras marcas y equipos clónicos de la mejor calidad, siempre contando con nuestro asesoramiento, 
-						para asegurarse de que todas sus máquinas se ajustan exactamente a las tareas a las que se van a dedicar.</p>
-				<p> Ponemos a disposición de nuestros clientes un gran equipo humano que se hará cargo del estudio y diseño de su red, para darle la mejor solución integral.</p> 
-				<p>Diseñamos redes que involucran la tecnología más avanzada con una excelente relación costo/beneficio.</p>
-				<p> Este servicio está disponible tanto para lograr el desarrollo de diseños iniciales o el rediseño de redes existentes.</p>
-			</div>
-		</div>
-		
-		<div id="servicios">
-			<div id="foto_servicios">
-			<img src="<?php echo base_url('images/02.jpg'); ?>" width="300" height="300" alt="02">
-			</div>
-			<div id="texto_servicios">
-				<h5>DESARROLLO DE SOFTWARE</h5>
-					<p>Muchas empresas necesitan del desarrollo de un software que permita automatizar las particularidades de la lógica del negocio 
-						y la integración de las múltiples aplicaciones que utilizan en el desarrollo de su actividad.</p>
-					<p> Nuestro alto nivel organizativo y metodológico nos convierten en una empresa de desarrollo Web capaz de adaptar
-					 los diferentes software del mercado en software a medida con gran seguridad y eficiencia.</p>
-					<p>Disponemos de un conjunto de soluciones tecnológicas y servicios de valor añadido pensados y desarrollados
-						 específicamente para dotar a cualquier Administración  pública de las herramientas necesarias para portar, mantener 
-						y agilizar todos y cada uno de los procesos que actualmente ofrece la Administración de forma presencial y realizarlos on-line. </p>
-					<p>Creamos  una plataforma web de información y servicios útil a la ciudadanía disponible las 24 horas del día y los 365 días del año.</p>
-			</div>
-		</div>
-		
-		<div id="servicios">
-			<div id="foto_servicios">
-			<img src="<?php echo base_url('images/03.jpg'); ?>" width="300" height="300" alt="03">
-			</div>
-			<div id="texto_servicios">
-				<h5>SERVICIOS WEB</h5>
-					<p>Aportamos soluciones de Diseño Web creativas para lograr una agradable y efectiva presencia en internet.</p>
-					<p> Nuestro equipo de diseñadores y técnicos informáticos son capaces aportar novedosas ideas y diseños teniendo 
-						siempre en cuenta la estrategia digital de la empresa. Podemos ofrecer las mejores soluciones del mercado para tener
-						 un diseño Web creativo y eficiente; disponemos desde las soluciones más económicas y sencillas para páginas Web hasta
-						 el diseño de portales con contenidos dinámicos.</p> 
-						<p>En Conectando Local cada proyecto Web, se realiza de forma personalizada y a medida del cliente.</p>
-						<p> Además ofrecemos servidores de alojamiento web seguros, estables y de última generación, con mantenimiento 
-							telefónico para solucionar instantáneamente cualquier pequeña incidencia.</p>
-			</div>
-		</div>
-		
-		<div id="servicios">
-			<div id="foto_servicios">
-			<img src="<?php echo base_url('images/04.jpg'); ?>" width="300" height="300" alt="04">
-			</div>
-			<div id="texto_servicios">
-				<h5>CONSULTORÍA INFORMÁTICA</h5>
-					<p>Somos conscientes de lo necesario que resulta en toda organización una correcta alineación de los procesos de negocio con la infraestructura tecnológica.</p>
-					<p>Disponemos de un área de proyectos de consultoría tecnológica en los que, tomando como punto de partida sus procesos empresariales, 
-						identificamos aquellos puntos críticos mejorables a través de automatización de procesos, con el fin de mejorar su productividad.</p>
-					<p>•	Estudio rápido y exhaustivo con toda la información cedida por el cliente.</p>
-					<p>•	Profesionalidad y eficacia, explicando de forma práctica las posibles mejoras.</p>
-					<p>•	Análisis completo de los recursos tecnológicos de la empresa, nos desplazamos al lugar donde se encuentre el cliente.</p>
-			</div>
-		</div>
-		
-		<div id="servicios">
-			<div id="foto_servicios">
-			<img src="<?php echo base_url('images/05.jpg'); ?>" width="300" height="300" alt="05">
-			</div>
-			<div id="texto_servicios">
-				<h5>VENTA DE EQUIPOS</h5>
-					<p>La política de calidad de nuestra empresa está orientada a satisfacer las necesidades de nuestros clientes, por eso 
-						garantizamos una amplia gama de productos y de servicios dentro de las principales marcar y distribuidores para elegir 
-						el producto que mejor se adapte a sus necesidades y a sus exigencias.</p>
-			</div>
-		</div>
-		
-		<div class="clear"></div>
-		<div id="barrita_azul"> 
-		</div>
+<div id="barrita"></div>
+<div id="servicios">
+	<div id="foto_servicios">
+		<img src="<?php echo base_url('images/01.jpg'); ?>" width="300" height="300" alt="01">
+	</div>
+	<div id="texto_servicios">
+	<h5>INSTALACIÓN Y MANTENIMIENTO DE EQUIPOS INFORMÁTICOS Y REDES</h5>
+		<p>Le asistimos en la configuración de su red local y su acceso a Internet para que sea seguro. 
+			No nos dedicamos a la simple distribución de equipos, sino que analizamos correctamente sus 
+			necesidades informáticas y realizamos un plan de compras con la vista puesta en el futuro: actualización, escalabilidad, rendimiento.</p> 
+		<p>Distribuimos primeras marcas y equipos clónicos de la mejor calidad, siempre contando con nuestro asesoramiento, 
+			para asegurarse de que todas sus máquinas se ajustan exactamente a las tareas a las que se van a dedicar.</p>
+		<p> Ponemos a disposición de nuestros clientes un gran equipo humano que se hará cargo del estudio y diseño de su red, para darle la mejor solución integral.</p> 
+		<p>Diseñamos redes que involucran la tecnología más avanzada con una excelente relación costo/beneficio.</p>
+		<p> Este servicio está disponible tanto para lograr el desarrollo de diseños iniciales o el rediseño de redes existentes.</p>
+	</div>
+</div>
+<div id="servicios">
+	<div id="foto_servicios">
+	<img src="<?php echo base_url('images/02.jpg'); ?>" width="300" height="300" alt="02">
+	</div>
+	<div id="texto_servicios">
+		<h5>DESARROLLO DE SOFTWARE</h5>
+			<p>Muchas empresas necesitan del desarrollo de un software que permita automatizar las particularidades de la lógica del negocio 
+				y la integración de las múltiples aplicaciones que utilizan en el desarrollo de su actividad.</p>
+			<p> Nuestro alto nivel organizativo y metodológico nos convierten en una empresa de desarrollo Web capaz de adaptar
+			 los diferentes software del mercado en software a medida con gran seguridad y eficiencia.</p>
+			<p>Disponemos de un conjunto de soluciones tecnológicas y servicios de valor añadido pensados y desarrollados
+				 específicamente para dotar a cualquier Administración  pública de las herramientas necesarias para portar, mantener 
+				y agilizar todos y cada uno de los procesos que actualmente ofrece la Administración de forma presencial y realizarlos on-line. </p>
+			<p>Creamos  una plataforma web de información y servicios útil a la ciudadanía disponible las 24 horas del día y los 365 días del año.</p>
+	</div>
+</div>
+<div id="servicios">
+	<div id="foto_servicios">
+	<img src="<?php echo base_url('images/03.jpg'); ?>" width="300" height="300" alt="03">
+	</div>
+	<div id="texto_servicios">
+		<h5>SERVICIOS WEB</h5>
+			<p>Aportamos soluciones de Diseño Web creativas para lograr una agradable y efectiva presencia en internet.</p>
+			<p> Nuestro equipo de diseñadores y técnicos informáticos son capaces aportar novedosas ideas y diseños teniendo 
+				siempre en cuenta la estrategia digital de la empresa. Podemos ofrecer las mejores soluciones del mercado para tener
+				 un diseño Web creativo y eficiente; disponemos desde las soluciones más económicas y sencillas para páginas Web hasta
+				 el diseño de portales con contenidos dinámicos.</p> 
+			<p>En Conectando Local cada proyecto Web, se realiza de forma personalizada y a medida del cliente.</p>
+			<p> Además ofrecemos servidores de alojamiento web seguros, estables y de última generación, con mantenimiento 
+					telefónico para solucionar instantáneamente cualquier pequeña incidencia.</p>
+	</div>
+</div>
+
+<div id="servicios">
+	<div id="foto_servicios">
+	<img src="<?php echo base_url('images/04.jpg'); ?>" width="300" height="300" alt="04">
+	</div>
+	<div id="texto_servicios">
+		<h5>CONSULTORÍA INFORMÁTICA</h5>
+			<p>Somos conscientes de lo necesario que resulta en toda organización una correcta alineación de los procesos de negocio con la infraestructura tecnológica.</p>
+			<p>Disponemos de un área de proyectos de consultoría tecnológica en los que, tomando como punto de partida sus procesos empresariales, 
+				identificamos aquellos puntos críticos mejorables a través de automatización de procesos, con el fin de mejorar su productividad.</p>
+			<p>•	Estudio rápido y exhaustivo con toda la información cedida por el cliente.</p>
+			<p>•	Profesionalidad y eficacia, explicando de forma práctica las posibles mejoras.</p>
+			<p>•	Análisis completo de los recursos tecnológicos de la empresa, nos desplazamos al lugar donde se encuentre el cliente.</p>
+	</div>
+</div>
+<div id="servicios">
+	<div id="foto_servicios">
+	<img src="<?php echo base_url('images/05.jpg'); ?>" width="300" height="300" alt="05">
+	</div>
+	<div id="texto_servicios">
+		<h5>VENTA DE EQUIPOS</h5>
+			<p>La política de calidad de nuestra empresa está orientada a satisfacer las necesidades de nuestros clientes, por eso 
+				garantizamos una amplia gama de productos y de servicios dentro de las principales marcar y distribuidores para elegir 
+				el producto que mejor se adapte a sus necesidades y a sus exigencias.</p>
+	</div>
+</div>
+<div class="clear"></div>
+<div id="barrita_azul"></div>
